b126b5493154b31b1fd41405bdb6f93efbdaf276
John Adolfo <>
2018-06-08 17:04:11 +1000

    Major revamp in security

---
+++ src/main/java/com/nutritionalStylist/healthyKitch/controller/AuthenticationController.java
@@ -0,0 +1,40 @@
+package com.nutritionalStylist.healthyKitch.controller;
+
+
+import com.nutritionalStylist.healthyKitch.config.security.TokenProvider;
+import com.nutritionalStylist.healthyKitch.model.security.AuthToken;
+import com.nutritionalStylist.healthyKitch.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/token")
+public class AuthenticationController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private TokenProvider jwtTokenUtil;
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(value = "/generate-token", method = RequestMethod.POST)
+    public ResponseEntity<?> register(@RequestParam String username, @RequestParam String password) throws AuthenticationException {
+
+        final Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        final String token = jwtTokenUtil.generateToken(authentication);
+        return ResponseEntity.ok(new AuthToken(token));
+    }
+
+}
